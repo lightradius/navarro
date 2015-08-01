6685d6878b84014782967819719d3e283012f39a
lightradius <hi@lightradius.com>
2015-08-01 16:45:36 +0100

    feat: adds direct link to unsplash art

--- app/components/home/home.ctrl.js
+++ app/components/home/home.ctrl.js
@@ -19,29 +19,31 @@
     function Controller($scope) {
         var vm = this;
 
+        vm.imageSrc = "https://download.unsplash.com/1/work-stations-plus-espresso.jpg";
+
         vm.strings = [
-            "lightradius is an independent web development brand, focused on consistency, simplicity and ease of use",
-            "specialized in both business and personal landing pages",
-            "sleek and simple designs, built using true and tested web frameworks",
-            "got an idea for a single page web application? Send us a holler and we can discuss it",
-            "thank you for your time!",
-            "scroll down for more infromation or click the button below to go to directly to my contacts section.",
-            ":)",
-            "that's all folks!",
-            "...",
-            "so... see you later I guess.",
-            "*sigh*",
-            "seriously, stop reading this, you're just wasting your time.",
-            "damn tourists.",
-            "if you must know, I absolutely hate the \"Keep Calm\" meme you can see printed on that black mug in the background image.",
-            "but this was the best royalty free picture of a laptop I could find on Unsplash...",
-            "so, no, those are not my hands and I do not use Brackets.",
-            "not that there's anything wrong with it.",
-            "...",
-            "if you have a Facebook account I will automatically assume you are a moron.",
-            "just kidding.",
-            "i have to return some videotapes.",
-            "go away."
+            // "lightradius is an independent web development brand, focused on consistency, simplicity and ease of use",
+            // "specialized in both business and personal landing pages",
+            // "sleek and simple designs, built using true and tested web frameworks",
+            // "got an idea for a single page web application? Send us a holler and we can discuss it",
+            // "thank you for your time!",
+            // "scroll down for more infromation or click the button below to go to directly to my contacts section.",
+            // ":)",
+            // "that's all folks!",
+            // "...",
+            // "so... see you later I guess.",
+            // "*sigh*",
+            // "seriously, stop reading this, you're just wasting your time.",
+            // "damn tourists.",
+            // "if you must know, I absolutely hate the \"Keep Calm\" meme you can see printed on that black mug in the background image.",
+            // "but this was the best royalty free picture of a laptop I could find on Unsplash...",
+            // "so, no, those are not my hands and I do not use Brackets.",
+            // "not that there's anything wrong with it.",
+            // "...",
+            // "if you have a Facebook account I will automatically assume you are a moron.",
+            // "just kidding.",
+            // "i have to return some videotapes.",
+            // "go away."
         ];
 
         activate();
@@ -52,7 +54,10 @@
                 typeSpeed: 0
             });
 
-            $('.parallax-window').parallax({imageSrc: '/assets/img/background.png'});
+            $('.parallax-window').parallax({
+                imageSrc: vm.imageSrc,
+                position: "top left",
+            });
         }
     }
 })();
